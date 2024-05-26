6b8eec25ec1dafaad3747173e09d934f2abcb597
Your Name <you@example.com>
2024-05-26 10:01:31 +0700

    rebuild base

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const mongoose = require("mongoose");
-const routes = require('./routes')
+const routes = require("./src/routes")
 const cors = require('cors');
-const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 dotenv.config()
@@ -13,8 +12,7 @@ const port = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
-app.use(bodyParser.json())
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 
 routes(app);
@@ -24,8 +22,9 @@ mongoose.connect(`${process.env.MONGO_DB}`)
         console.log('Connect Db success!')
     })
     .catch((err) => {
-        // console.log(err)
+        console.log(err)
     })
+
 app.listen(port, () => {
-    console.log('Server is running in port: ', + port)
-})
+    console.log('Server is running in port:', port);
+});
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,13 @@
+const userRouter = require('./userRoutes')
+const medicineRouter = require('./medicineRoutes')
+const saleRouter = require('./saleRoutes')
+const supplierRouter = require('./supplierRoutes')
 
 const routes = (app) => {
-    app.use('/api/jobs', JobRouter)
-    app.use('/api/user', UserRouter)
+    // app.use('/api/medicine', medicineRouter)
+    app.use('/api/user', userRouter)
+    // app.use('/api/sale', saleRouter)
+    // app.use('/api/supplier', supplierRouter)
 }
 
 module.exports = routes
